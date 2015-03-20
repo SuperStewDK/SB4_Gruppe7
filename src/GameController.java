f465f00cf777c7735190b4f6883c85613c6df92f
bnjmlnk <benjaminmlynek4@gmail.com>
2015-03-20 14:36:55 +0100

    Added fields and methods.

--- src/GameController.java
+++ src/GameController.java
@@ -2,4 +2,19 @@
  * Created by Steffen on 20-03-2015.
  */
 public class GameController {
+
+    int frameRate
+    long time
+
+    private GameController gameController(){
+
+    }
+
+    public static GameController getInstance() {
+
+    }
+
+    public long getDelta() {
+
+    }
 }
