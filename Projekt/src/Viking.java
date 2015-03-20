21d8de41874004b4e78a5a87d5b3f5f2c40f7686
SuperStewDK <stew-1989@hotmail.com>
2015-03-20 15:07:15 +0100

    Added fields and methods.

--- Projekt/src/Viking.java
+++ Projekt/src/Viking.java
@@ -2,5 +2,10 @@
  * Created by Steffen on 20-03-2015.
  */
 public class Viking extends Entity {
+    private double speed;
+    private double xPosition;
+    private double yPosition;
+    private boolean canAttack;
+    private double facing;
 
 }
