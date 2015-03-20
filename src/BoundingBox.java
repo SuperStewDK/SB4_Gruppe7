547955e82fd348d5760f353e42a29a396518b1fe
SuperStewDK <stew-1989@hotmail.com>
2015-03-20 14:23:08 +0100

    entity attributes added

--- src/BoundingBox.java
+++ src/BoundingBox.java
@@ -3,5 +3,5 @@
  */
 public class BoundingBox {
 
-    Viking viking = new Viking();
+
 }
--- src/Entity.java
+++ src/Entity.java
@@ -2,4 +2,9 @@
  * Created by Steffen on 20-03-2015.
  */
 public class Entity {
+    double speed;
+    double xPosition;
+    double yPosition;
+    Boolean canAttack;
+    double facing;
 }
