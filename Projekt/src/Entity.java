786346742600c4aaf17c94e5680f0a0c680c48de
SuperStewDK <stew-1989@hotmail.com>
2015-03-20 15:15:30 +0100

    Entity interface lavet

--- Projekt/src/Entity.java
+++ Projekt/src/Entity.java
@@ -1,11 +1,17 @@
 /**
  * Created by Steffen on 20-03-2015.
  */
-public abstract class Entity {
-    private double speed;
-    private double xPosition;
-    private double yPosition;
-    private boolean canAttack;
-    private double facing;
+public interface Entity {
+    double speed = 0;
+    double xPosition = 0;
+    double yPosition = 0;
+    boolean canAttack = false;
+    double facing = 0;
+
+    public void onHit();
+
+    public void update(double deltaT);
+
+    public void move(double deltaT);
 
 }
