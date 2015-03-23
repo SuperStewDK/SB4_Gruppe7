d9a5246d838b90d02e9d82160c37779be8b605ef
bnjmlnk <benjaminmlynek4@gmail.com>
2015-03-23 16:11:35 +0100

    Added weapon functionality.

--- Projekt/src/Weapon.java
+++ Projekt/src/Weapon.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Steffen on 20-03-2015.
  */
@@ -8,6 +11,22 @@ public class Weapon implements IEntity {
     private double yPosition;
     private double facing;
 
+    private boolean visible;
+    private final int BOARD_WIDTH = 1000;
+    private final int MISSILE_SPEED = 2;
+    private Image image;
+    private int x,y;
+
+    private String axe = "/Users/benjaminmlynek/Coding/IdeaProjects/SB4_Gruppe7/Projekt/src/img/throwingaxe.jpg";
+
+    public Weapon(int x, int y) {
+        ImageIcon ii = new ImageIcon(axe);
+        image = ii.getImage().getScaledInstance(40, 30, Image.SCALE_SMOOTH);
+        visible = true;
+        this.x = x;
+        this.y = y;
+    }
+
     public void onHit() {
     }
 
@@ -15,5 +34,27 @@ public class Weapon implements IEntity {
     }
 
     public void move(double deltaT) {
+
+        x += MISSILE_SPEED;
+        if (x > BOARD_WIDTH){
+            visible = false;
+        }
+
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isVisible() {
+        return visible;
     }
 }
